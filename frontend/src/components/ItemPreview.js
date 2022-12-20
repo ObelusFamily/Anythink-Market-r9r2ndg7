000e6ed5f3208448a63209cb7132de76f90071c0
Karl Linfeldt <104808133+KarlOL82@users.noreply.github.com>
2022-12-20 04:27:15 +0000

    debugging image issue

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
+import { useState } from "react";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -29,6 +30,8 @@ const ItemPreview = (props) => {
     }
   };
 
+  
+
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -37,7 +40,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image} onError = {() => setImgSrc("../imgs/placeholder.png")}
+        src={item.image} 
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
